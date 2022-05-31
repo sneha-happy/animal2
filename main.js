2278406c6a87c8049b81c752b4199be874bf4dc4
sneha-happy <99965721+sneha-happy@users.noreply.github.com>
2022-05-31 17:46:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function Start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/2bnsx0TVk/model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error , results){
+    console.log('gotResults');
+}
